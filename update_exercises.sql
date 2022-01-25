aac6f0480da0f4567a05a3349027b12e2a178bcb
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-25 15:40:19 -0600

    Added query to display all albums release before 1980.

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,3 +3,6 @@ USE codeup_test_db;
 
 SELECT 'All albums in your table' AS 'INFO';
 SELECT id, name FROM albums;
+
+SELECT 'All albums released before 1980.';
+SELECT name, release_date FROM albums where release_date < 1980;
