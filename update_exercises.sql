58331decc7b188d1887f80f3bd3569f2ea203a25
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-25 16:06:39 -0600

    Added an UPDATE statement to move all albums before 1980 to the 1800s.

--- update_exercises.sql
+++ update_exercises.sql
@@ -7,10 +7,14 @@ SELECT id, name, sales FROM albums;
 SELECT 'All albums in your table with sales multiplied by 10' AS 'INFO';
 UPDATE albums SET sales = (sales * 10);
 SELECT id, name, sales FROM albums;
---
--- SELECT 'All albums released before 1980.';
--- SELECT name, release_date FROM albums where release_date < 1980;
---
+
+SELECT 'All albums released before 1980.' AS 'INFO';
+SELECT name, release_date FROM albums where release_date < 1980;
+
+SELECT 'Updated albums release before 1980 to be released before 1880.' AS 'INFO';
+UPDATE albums SET release_date = (release_date + 100);
+SELECT name, release_date FROM albums where release_date < 1880;
+
 -- SELECT 'All albums by Michael Jackson' AS 'INFO';
 -- SELECT name FROM albums where artist = 'Michael Jackson';
 
