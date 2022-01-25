c70fbbe04c1f37c59e19df65981424fad44bc3cc
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-25 16:31:20 -0600

    Added UPDATE statement to change 'Michael Jackson' to 'Peter Jackson'.

--- update_exercises.sql
+++ update_exercises.sql
@@ -12,9 +12,13 @@ SELECT 'All albums released before 1980.' AS 'INFO';
 SELECT name, release_date FROM albums where release_date < 1980;
 
 SELECT 'Updated albums release before 1980 to be released before 1880.' AS 'INFO';
-UPDATE albums SET release_date = (release_date + 100);
+UPDATE albums SET release_date = (release_date - 100) WHERE release_date < 1980;
 SELECT name, release_date FROM albums where release_date < 1880;
 
--- SELECT 'All albums by Michael Jackson' AS 'INFO';
--- SELECT name FROM albums where artist = 'Michael Jackson';
+SELECT 'All albums by Michael Jackson' AS 'INFO';
+SELECT name, artist FROM albums where artist = 'Michael Jackson';
+
+SELECT 'Update all albums by Michael Jackson to now display Peter Jackson' AS 'INFO';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT name, artist FROM albums WHERE artist = 'Peter Jackson';
 
