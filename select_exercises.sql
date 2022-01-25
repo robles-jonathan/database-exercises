b36cad53ae2d599c4fb6d4f905cd93c5a28c8ae6
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-25 15:12:11 -0600

    Added query to display the year Sgt. Pepper's Lonely Hearts Club Band was released.

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,6 @@ USE codeup_test_db;
 
 SELECT 'The name of all albums by Pink Floyd.' AS 'INFO';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released.' AS 'INFO';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
