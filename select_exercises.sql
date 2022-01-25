d34f2750312fd882e33bc00338d71c365f7c4eb1
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-25 15:17:20 -0600

    Added query to display which albums were released in the 1990s.

--- select_exercises.sql
+++ select_exercises.sql
@@ -11,3 +11,6 @@ SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club 
 
 SELECT 'The genre for Nevermind.' AS 'INFO';
 SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s.' AS 'INFO';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
