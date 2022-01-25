ab81c3b6ca5c58b5471e196fe56488e0a3b9b4ee
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-25 15:08:43 -0600

    Added query to display information of all albums by Pink Floyd.

---
+++ select_exercises.sql
@@ -0,0 +1,7 @@
+-- set by using the USE keyword the DATABASE to codeup_test_db
+USE codeup_test_db;
+-- To execute use mysql -u codeup_test_user -p -t < select_exercises.sql
+-- Output a caption explaining the results of query.
+
+SELECT 'The name of all albums by Pink Floyd.' AS 'INFO';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
