794406bfc6146c5d6be631e4952429dd39d590cc
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-26 12:34:58 -0600

    Created query for first names including Irena, Vidya, Maya.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,1 +1,6 @@
 USE employees;
+
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE first_name IN('Irena', 'Vidya') OR first_name IN('Maya');
