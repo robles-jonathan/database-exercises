6faeeafa080552ef814c7b976e76302c075161be
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-26 12:35:39 -0600

    Created query for employees whose last name starts with 'E'.

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,3 +4,8 @@ USE employees;
 SELECT *
 FROM employees
 WHERE first_name IN('Irena', 'Vidya') OR first_name IN('Maya');
+
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
