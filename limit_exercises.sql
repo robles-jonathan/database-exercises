e2f314735614d2fb1e0705d6e99db97fbcbb29a9
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-27 10:59:17 -0600

    Create a query to get the tenth page of results if each page of results had 5 entries.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,4 +7,6 @@ SELECT DISTINCT last_name From employees ORDER BY last_name DESC LIMIT 10;
 SELECT * FROM salaries;
 SELECT salary, emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
 
+SELECT salary, emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
+
 
