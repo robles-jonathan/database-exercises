892e4d66d3e8d457f0f77e9020b464279463ee6c
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-27 12:16:02 -0600

    Updated last query so that the first result will show the oldest employee who was hired last.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -53,4 +53,5 @@ SELECT *
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
 AND month(birth_date) = 12
-    AND day(birth_date) = 25;
+    AND day(birth_date) = 25
+ORDER BY birth_date, hire_date DESC;
