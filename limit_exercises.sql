5f7a2ae8495fa8d025dd15b6cf9979bf95a36ad5
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-27 10:54:22 -0600

    Create query to get top 5 salaries.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,5 +4,7 @@ SELECT DISTINCT title FROM titles;
 
 SELECT DISTINCT last_name From employees ORDER BY last_name DESC LIMIT 10;
 
+SELECT * FROM salaries;
+SELECT salary, emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
 
 
