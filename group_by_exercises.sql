796e47353912b1f23c0a6a5fa5381bb71050ce10
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-27 15:56:30 -0600

    updated query

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,6 +2,8 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT last_name FROM employees
+SELECT last_name, last_name
+FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY last_name;
+GROUP BY last_name, first_name
+ORDER BY last_name;
