3c3db023dbc6e4f7be078afe57ee62c8b13d9fb2
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-31 14:38:37 -0600

    Created query to get each department with the current department manager who is a female.

--- join_exercises.sql
+++ join_exercises.sql
@@ -6,3 +6,12 @@ FROM dept_manager as dm
               ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
+SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager"
+FROM dept_manager as dm
+         JOIN employees as e
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+ORDER BY d.dept_name;
