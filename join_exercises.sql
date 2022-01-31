082706a478a90f65ef4d939ad05d4f607435112f
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-31 14:35:32 -0600

    Created query to get each department with the current department manager.

---
+++ join_exercises.sql
@@ -0,0 +1,8 @@
+SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager"
+FROM dept_manager as dm
+         JOIN employees as e
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
